65410bfeab5c6ae03dc58c277430d075cd228bf2
shulme33 <shulme33@gmail.com>
2020-08-24 19:32:03 -0600

    Connected to local server

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,17 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 import "../css/banner.css";
 
-function Banner() {
-  return (
-    <div className="banner">
-      <p>We Are</p>
-      <h1>Streamline LLC.</h1>
-      <div className="banner-divider"></div>
-      <p>This site is still under construction</p>
-      <p>Check back later for updates</p>
-      <p>Another line</p>
-    </div>
-  );
+class Banner extends Component {
+  constructor() {
+    super();
+    this.state = { testMessage: "TEST" };
+  }
+
+  componentDidMount() {
+    console.log("Banner Component Mounted!");
+    fetch("/api/test")
+      .then((res) => res.json())
+      .then((incomingTestMessage) =>
+        this.setState({ testMessage: incomingTestMessage }, () =>
+          console.log("Skills fetched...", this.state.testMessage)
+        )
+      );
+  }
+
+  showServer() {
+    return <p>{this.state.testMessage}</p>;
+  }
+
+  render() {
+    return (
+      <div className="banner">
+        <p>We Are</p>
+        <h1>Streamline LLC.</h1>
+        <div className="banner-divider"></div>
+        <p>This site is still under construction</p>
+        <p>Check back later for updates</p>
+        {this.showServer()}
+        <button
+          onClick={() => {
+            console.log("Button Clicked");
+          }}
+        >
+          Click Me
+        </button>
+      </div>
+    );
+  }
 }
 
 export default Banner;
